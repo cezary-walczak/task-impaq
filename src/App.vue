c551324608e1046eef258fc2842c7a7eb252fd8f
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-06-07 21:48:40 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .app
+  
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
